9ac6db80f52999d7838439ac5dce398af57be13c
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2025-09-03 15:02:25 -0700

    Resolve unexpected randomness in tests for 0nu mode
    - Update EmpiricalDistributions to 0.3.9
    - Add back the 0nu test (it now passes with fixed randomness)

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,12 +13,11 @@ using Random
     @test E2 ≈ 1251.059139039826 atol=1e-15
     @test cosθ12 ≈ 0.9180839309783714 atol=1e-15
 
-    # XXX: For ZeroNuDBDData the test fails with Julia v1.11.6
-    #Random.seed!(rng, 999)
+    Random.seed!(rng, 999)
 
-    #dat_0nu = ZeroNuDBDData()
-    #E1, E2, cosθ12 = rand(rng, dat_0nu)
-    #@test E1 ≈ 1628.9956960725929 atol=1e-15
-    #@test E2 ≈ 410.00430392740714 atol=1e-15
-    #@test cosθ12 ≈ -0.7239804777193068 atol=1e-15
+    dat_0nu = ZeroNuDBDData()
+    E1, E2, cosθ12 = rand(rng, dat_0nu)
+    @test E1 ≈ 2016.2355832686694 atol=1e-15
+    @test E2 ≈ 22.764416731330584 atol=1e-15
+    @test cosθ12 ≈ -0.9113939723175647 atol=1e-15
 end
